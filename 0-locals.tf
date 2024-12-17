1efdb410b2cb07719dbd94eb30c3cffa13292502
Fellipe Bastos <fellipe.resende@hotmail.com>
2024-12-16 23:09:31 -0300

    ajustando cd/ci

--- 0-locals.tf
+++ 0-locals.tf
@@ -3,9 +3,8 @@ locals {
   # profile         = "fiap"
   region          = "us-east-1"
   vpc_id          = "vpc-008d410f1d34c3021"
-  security_group_id = "sg-0db5bdc61902e305c"
-  subnets_private = ["subnet-01e6440e656ec6092", "subnet-0314707bfb8f170ae"]
-  subnets_public  = ["subnet-0329c3cf38b32e8fe", "subnet-0b5794ae05e699b02"]
+  subnets_private = ["subnet-0329c3cf38b32e8fe", "subnet-0b5794ae05e699b02"]
+  subnets_public  = ["subnet-0f34dc9eef5accee2", "subnet-034fa8fd296e5d02f"]
   sg_name = "rds-sg"
   rds_name = "lanchonetedb"
   rds_engine = "postgres"
